a9926bffb6250e0cb6c4f2dbc6f4861b3b600523
farhanpatel <farhanpatel.17@gmail.com>
2017-01-24 11:27:01 -0800

    A modified webpack config for ios9 and ios10 using babel-tranform

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,8 @@ module.exports = {
     externals: {
       url: 'window',
     },
-    output: {
         path: './client',
-        filename: 'page-metadata-parser.bundle.js',
+        filename: 'page-metadata-parser.js',
         libraryTarget: 'var',
         library: 'metadataparser'
     },
@@ -14,8 +13,14 @@ module.exports = {
             test: /\.js$/,
             loader: 'babel-loader',
             query: {
-              presets: ['es2015']
-            }
+              presets: ['es2015'],
+              plugins: ['transform-es2015-constants', 'transform-runtime']
+            },
+            exclude: [
+                /node_modules\/babel-/m,
+                /node_modules\/core-js\//m,
+                /node_modules\/regenerator-runtime\//m
+            ]
         }]
     }
 };
