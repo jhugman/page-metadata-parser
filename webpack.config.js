41214e4d456a11c04571ab44b454a2e53e465289
James Hugman <james@hugman.tv>
2017-10-04 17:32:10 +0100

    Modify babelrc to use iOS10 specific presets.

--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,6 @@ module.exports = {
         loaders: [{
             test: /\.js$/,
             loader: 'babel-loader',
-            query: {
-              presets: ['es2015'],
-              plugins: ['transform-es2015-constants', 'transform-runtime']
-            },
             exclude: [
                 /node_modules\/babel-/m,
                 /node_modules\/core-js\//m,
